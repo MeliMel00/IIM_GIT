42d86eeb20e24ce189899347ae3d2dc4217a673f
Mel <melvin.chevallier@edu.devinci.fr>
2023-10-16 12:25:23 +0200

    Finalisation du Login. Fix #1

--- login.php
+++ login.php
@@ -17,9 +17,14 @@ if(isset($_POST['email']) && isset($_POST['password'])){
 		// TODO
 
 		// Force user connection to access dashboard
-		userConnection($db, 'git@initiation.com', 'password');
+		// userConnection($db, 'git@initiation.com', 'password');
 		
-		header('Location: dashboard.php');
+		if (userConnection($db, $_POST['email'], $_POST['password'])) {
+			header('Location: dashboard.php');
+		}
+		else{
+			$error = 'Mauvais identifiants';
+		}
 
 	}else{
 		$error = 'Champs requis !';
